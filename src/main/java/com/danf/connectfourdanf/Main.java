e277769c4acad9f223a4796005b057dd0fec4944
CaptSlow <daniel.forrester@live.co.uk>
2019-05-19 16:12:25 +0100

    ran code formatting

--- src/main/java/com/danf/connectfourdanf/Main.java
+++ src/main/java/com/danf/connectfourdanf/Main.java
@@ -2,8 +2,8 @@ package com.danf.connectfourdanf;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Starting Connect Four");
-    }
+  public static void main(String[] args) {
+    System.out.println("Starting Connect Four");
+  }
 
 }
